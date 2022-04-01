99e0c992eebdbcddfc0e8aefe28cde8984f1ecc8
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-04-01 22:47:53 +0700

    api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="wrapper">
+            <div class="content" v-for="(item, index) in products" :key="index">
+                <div class="thumb">
+                    <img :src="item.thumb" alt="">
+                </div>
+                <div class="name">{{item.name}}</div>
+                <div class="desc">{{item.description}}</div>
+                <div class="price">{{item.price}}</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    // import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {},
+        data() {
+            return {
+                products: [],
+                valueInput: "",
+                url: `https://6246a3c3e3450d61b000fdf0.mockapi.io/products`,
+                options: {method: 'GET'},
+            }
+        },
+        method: {
+            api() {
+                fetch(this.url).then((res) => {
+                    return res.json()
+                }).then((data) => {
+                    this.products = data
+                }).catch((err) => console.log(err))
+            }
+        },
+        mounted() {
+            this.api()
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
